4493b27a61b8acfac02c51d71e269739f0c8a356
velouria <montoyamoraga@gmail.com>
2020-06-19 00:13:36 -0400

    agregar static paths y p5 sketch

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,3 +36,20 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# copied from https://stackoverflow.com/questions/31270373/how-to-add-a-favicon-to-a-pelican-blog
+STATIC_PATHS = [
+    'images',
+    'extra',
+]
+
+# copied from https://stackoverflow.com/questions/31270373/how-to-add-a-favicon-to-a-pelican-blog
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'custom.css'},
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/sketch.js': {'path': 'sketch.js'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},  # and this
+    'extra/CNAME': {'path': 'CNAME'},
+    'extra/LICENSE': {'path': 'LICENSE'},
+    'extra/README': {'path': 'README'},
+}
